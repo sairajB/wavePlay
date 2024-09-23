0b6517c84427724348bf29ec3b92fc93258398cd
Sairaj Bodhale <sairaj.sab@gmail.com>
2024-09-23 15:57:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,5 +120,5 @@ def stop_video():
     streaming = False
     return '', 204
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
